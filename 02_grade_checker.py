c4095eb708307f038a04c66e8c2923967b64af78
Riley Scott <sco6028@swtc.edu>
2026-08-25 12:56:19 -0500

    first commit

---
+++ 02_grade_checker.py
@@ -0,0 +1,16 @@
+class_grade = (int(input("What is your grade percentage in math? ")))
+
+if class_grade >= 90:
+    print("You have an A in math!")
+
+elif class_grade >= 80:
+    print("You have an B in math!")
+
+elif class_grade >= 70:
+    print("You have an C in math!")
+
+elif class_grade >= 60:
+    print("You have an D in math!")
+
+else:
+    print("You are failing math!")
